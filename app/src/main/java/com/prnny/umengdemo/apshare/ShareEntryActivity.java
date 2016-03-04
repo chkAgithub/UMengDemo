506e59f54dae2c6e97b65c7ed66b90af92e75d8d
Prnny <prnny4ck@sina.com>
2016-03-04 15:22:50 +0800

    init commit

---
+++ app/src/main/java/com/prnny/umengdemo/apshare/ShareEntryActivity.java
@@ -0,0 +1,9 @@
+package com.prnny.umengdemo.apshare;
+
+import com.umeng.socialize.media.ShareCallbackActivity;
+
+/**
+ * Created by Prnny on 2016/3/3.
+ */
+public class ShareEntryActivity extends ShareCallbackActivity{
+}
